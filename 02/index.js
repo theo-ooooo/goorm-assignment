3549003fb1ce770d21c5ff0ad74e4b1cddc69a60
theo-ooooo <kkw.theo@gmail.com>
2025-03-14 16:25:52 +0900

    add:수정정

--- 02/index.js
+++ 02/index.js
@@ -44,8 +44,20 @@ class Todo {
   }
 
   handleTodoWrapClick(event) {
-    const todoElement = event.target.parentElement;
-    this.deleteTodo(todoElement.id);
+    const todoElement = event.target.parentElement.parentElement;
+    const target = event.target;
+
+    const classList = Array.from(target.classList);
+
+    const todoId = todoElement.id;
+    const todo = this.todos.find((todo) => todo.id === todoId);
+
+    if (classList.includes("delete_btn")) {
+      console.log(todoElement, todoId);
+      this.deleteTodo(todoId);
+    } else if (classList.includes("edit_btn")) {
+      this.editMode(todoElement, todo);
+    }
   }
 
   addTodo(text) {
@@ -56,6 +68,18 @@ class Todo {
     };
 
     this.todos = [...this.todos, todo];
+    this.saveByLocalStorage();
+    this.renderTodos();
+  }
+
+  editTodo(id, newText) {
+    this.todos = this.todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, text: newText };
+      }
+      return todo;
+    });
+
     this.saveByLocalStorage();
     this.renderTodos();
   }
@@ -82,6 +106,36 @@ class Todo {
     this.renderTodos();
   }
 
+  editMode(todoItemElement, todo) {
+    const inputDiv = document.createElement("div");
+    const inputElement = document.createElement("input");
+    inputDiv.classList.add("input");
+    inputElement.type = "text";
+    inputElement.id = "todo";
+    inputElement.classList.add("add_input");
+    inputElement.value = todo.text;
+
+    const handleInput = () => {
+      const value = inputElement.value.trim();
+      if (value) {
+        this.editTodo(todoItemElement.id, value);
+      }
+
+      inputDiv.remove();
+    };
+
+    inputElement.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleInput();
+      }
+    });
+
+    inputDiv.appendChild(inputElement);
+    this.todoWarp.appendChild(inputDiv);
+    inputElement.focus();
+  }
+
   renderTodos() {
     this.todoWarp.innerHTML = "";
     this.todos.forEach((todo) => {
@@ -96,12 +150,23 @@ class Todo {
       todoElement.appendChild(todoText);
       this.todoWarp.appendChild(todoElement);
 
+      // 버튼들
+      const buttons = document.createElement("div");
+      buttons.classList.add("buttons");
+
+      //수정정 버튼
+      const editButtonElement = document.createElement("button");
+      editButtonElement.classList.add("edit_btn");
+      editButtonElement.innerText = "수정";
+      buttons.appendChild(editButtonElement);
+
       //삭제 버튼
       const deleteButtonElement = document.createElement("button");
       deleteButtonElement.classList.add("delete_btn");
       deleteButtonElement.innerText = "삭제";
+      buttons.appendChild(deleteButtonElement);
 
-      todoElement.appendChild(deleteButtonElement);
+      todoElement.appendChild(buttons);
     });
   }
 }
